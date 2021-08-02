085fdd3cf458d80da160db17d75f98d195033bc1
unknown <sivaranjanib99@gmail.com>
2021-08-02 08:50:05 +0530

    [siva]UC3 inserting into the table

--- AddressBook_services.sql
+++ AddressBook_services.sql
@@ -14,3 +14,9 @@ create table AddressBookTable
   phoneNumber bigint,
   email varchar(100)
 )
+--UC3-Inserting new rows inside the table--
+insert into AddressBookTable values('Siva','Ranjani','East new st','madurai','Tamil Nadu',600259,8667361462,'siva@gmail.com'),
+('Bala','Vickram','cross street','chennai','Tamil Nadu',600145,9786146251,'bala@gmail.com'),
+('Karthik','Bala','KKnagar','Trichy','Tamil Nadu',600189,9861325453,'karthick@gmail.com'),
+('Siva','Karthik','Anna nagar','salem','Tamil Nadu',600019,789643210,'sikar@gmail.com')
+select * from AddressBookTable
