11abb353ad876dadb91350c0c37a4ef3e44ba839
unknown <sivaranjanib99@gmail.com>
2021-08-02 07:53:49 +0530

    [siva]UC2 creating the table

--- AddressBook_services.sql
+++ AddressBook_services.sql
@@ -1,3 +1,16 @@
 --UC1-Creating the database for addressbook services--
 create database Addressbook_services;
 use  Addressbook_services;
+--UC2- creating table-
+create table AddressBookTable
+(
+  personId int identity(1,1) primary key,
+  firstName varchar(200),
+  lastName varchar(200),
+  address varchar(200),
+  city varchar(100),
+  state varchar(100),
+  zipCode bigint,
+  phoneNumber bigint,
+  email varchar(100)
+)
